0a22df53b1d8f961513c67521d9dde5fc3ffa6e5
Mary Marks <maryleemarks@gmail.com>
2021-08-04 14:50:37 -0700

    get tests to 100% coverage

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -2,70 +2,79 @@ import Age from './../src/age.js';
 
 describe('Age', () => {
 
-  let age;
-
-  beforeEach(() => {
-    age = new Age(33, 20);
-  });
-
   test('creates a new constructor with properties age and life expectancy', () => {
+    const age = new Age(33, 75);
     expect(age.earthAge).toEqual(33);
-    expect(age.lifeExpectancy).toEqual(20);
+    expect(age.lifeExpectancy).toEqual(75);
   });
 
   test('returns the correct age for Mercury', () => {
+    const age = new Age(33, 75);
     expect(age.mercuryAge()).toEqual(137);
   });
 
   test('returns the correct age for Venus', () => {
+    const age = new Age(33, 75);
     expect(age.venusAge()).toEqual(53);
   });
 
-  test('returns the correct age for Mars', () => {
+ test('returns the correct age for Mars', () => {
+    const age = new Age(33, 75);
     expect(age.marsAge()).toEqual(17);
   });
 
   test('return the correct age for Jupiter', () => {
+    const age = new Age(33, 75);
     expect(age.jupiterAge()).toEqual(2);
   });
 
   test('returns the correct number of earth years remaining according to user input of life expectancy', () => {
-    expect(age.earthLifeLeft()).toEqual(13);
+    const age = new Age(33, 75);
+    expect(age.earthLifeLeft()).toEqual(42);
   });
 
   test('returns the number of years lived past life expectancy if life expectancy has been exceeded', () => {
-    expect(age.earthLifeLeft()).toEqual(13);
+    const age = new Age(75, 33);
+    expect(age.earthLifeLeft()).toEqual(42);
   });
 
   test('return the number of Mercury years remaining', () => {
-    expect(age.mercuryLifeLeft()).toEqual(54) 
-  })
+    const age = new Age(33, 75);
+    expect(age.mercuryLifeLeft()).toEqual(175) 
+  });
 
   test('return the number of Mercury years lived past life expectancy if life expectancy is exceeded', () => {
-    expect(age.mercuryLifeLeft()).toEqual(54)
-  })
+    const age = new Age(75, 33);
+    expect(age.mercuryLifeLeft()).toEqual(175)
+  });
 
   test('return the number of Venus years remaining', () => {
-    expect(age.venusLifeLeft()).toEqual(20)
-  })
+    const age = new Age(33, 75);
+    expect(age.venusLifeLeft()).toEqual(67)
+  });
 
   test('return the number of Venus years lived past life expectancy if life expectancy is exceeded', () => {
-    expect (age.venusLifeLeft()).toEqual(20)
-  })
+    const age = new Age(75, 33);
+    expect(age.venusLifeLeft()).toEqual(67)
+  });
 
   test('return the number of Mars years remaining', () => {
-    expect(age.marsLifeLeft()).toEqual(6)
-  })
+    const age = new Age(33, 75);
+    expect(age.marsLifeLeft()).toEqual(22)
+  });
 
   test('return the number of Mars years lived past life expectancy if life expectancy is exceeded', () => {
-    expect (age.marsLifeLeft()).toEqual(6)
-  })
+    const age = new Age(75, 33);
+    expect (age.marsLifeLeft()).toEqual(22)
+  });
 
   test('return the number of Jupiter years remaining', () => {
-    expect(age.jupiterLifeLeft()).toEqual(1)
-  })
+    const age = new Age(33, 75);
+    expect(age.jupiterLifeLeft()).toEqual(3)
+  });
 
   test('return the number of Jupiter years lived past life expectancy if life expectancy is exceeded', () => {
-    expect (age.jupiterLifeLeft()).toEqual(1)
-  })
+    const age = new Age(75, 33);
+    expect(age.jupiterLifeLeft()).toEqual(3)
+  });
 });
